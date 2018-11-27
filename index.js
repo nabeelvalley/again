ce00477f2a1c84ed30462b72e57c6f6e5c4c39f2
Nabeel Valley <nabeelvalley1@hotmail.com>
2018-11-27 17:38:06 +0200

    add functionality to specify watch directory

--- index.js
+++ index.js
@@ -1,23 +1,97 @@
 #!/usr/bin/env node
-var app = require('commander')
+const app = require('commander')
+const chokidar = require('chokidar')
+const child_process = require('child_process')
 
-console.log('Hello, startover, and startover..')
+// Utility functions
+const split = val => val.split(',')
 
+const execute = () => {
+    command = app.commands.join(' && ')
+    child_process.exec(command, function(error, stdout, stderr) {
+        if (stdout) console.log('command out:\n ' + stdout)
+        if (stderr) console.log('stderr:\n' + stderr)
+        if (error !== null) console.log('exec error: ' + error)
+    })
+}
+
+//  Define Parameters
 app.version('0.1.0')
-    .option('-p, --peppers', 'Add peppers')
-    .option('-P, --pineapple', 'Add pineapple')
-    .option('-b, --bbq-sauce', 'Add bbq sauce')
     .option(
-        '-c, --cheese [type]',
-        'Add the specified type of cheese [marble]',
-        'marble'
+        '-d, --watch-dirs [directories]',
+        'List of directories to watch, by default will watch current directory',
+        split
+    )
+    .option(
+        '-c, --commands [directory]',
+        'List of Commands to run when files change',
+        split
+    )
+    .option(
+        '-f, --exclude-files [files to exclude]',
+        'List of Files to Exclude from Watch',
+        split
+    )
+    .option(
+        '-e, --exclude-extensions [extensions to exclude]',
+        'List of Extensions to Exclude from Watch',
+        split
+    )
+    .option(
+        '-D, --exclude-directories [directories to exclude]',
+        'List of Directories to Exclude from Watch',
+        split
     )
     .parse(process.argv)
 
-console.log('you ordered a pizza with:')
-if (app.peppers) console.log('  - peppers')
-if (app.pineapple) console.log('  - pineapple')
-if (app.bbqSauce) console.log('  - bbq')
-console.log('  - %s cheese', app.cheese)
+console.log('Called with the following Options')
 
-// console.log(app)
+if (app.watchDirs) console.log('watch dirs:', app.watchDirs)
+if (app.commands) console.log('commands:', app.commands)
+if (app.excludeFiles) console.log('excluded files:', app.excludeFiles)
+if (app.excludeExtensions)
+    console.log('excluded extensions:', app.excludeExtensions)
+if (app.excludeDirectories)
+    console.log('excluded directories:', app.excludeDirectories)
+
+// Initialize watcher.
+var watcher = chokidar.watch(app.watchDirs ? app.watchDirs : '.', {
+    ignored: new Array().concat(
+        app.excludeDirectories,
+        app.excludeFiles,
+        new RegExp(app.excludeExtensions.map(el => '\.' + el).join('|')),
+        /(^|[\/\\])\../
+    ),
+    persistent: true
+})
+
+// Something to use when events are received.
+var log = console.log.bind(console)
+
+// Add event listeners.
+
+var ready = false
+
+watcher
+    .on('add', path => console.log(`File ${path} has been added`))
+    .on('change', path => {
+        if (ready) execute()
+        log(`File ${path} has been changed`)
+    })
+    .on('unlink', path => {
+        if (ready) execute()
+        log(`File ${path} has been removed`)
+    })
+    .on('addDir', path => {
+        if (ready) execute()
+        log(`Directory ${path} has been added`)
+    })
+    .on('unlinkDir', path => {
+        if (ready) execute()
+        log(`Directory ${path} has been removed`)
+    })
+    .on('error', error => log(`Watcher error: ${error}`))
+    .on('ready', () => {
+        ready = true
+        log('Initial scan complete. Ready for changes')
+    })
